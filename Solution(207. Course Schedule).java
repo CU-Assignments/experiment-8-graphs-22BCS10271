5f2bab46e2c18cd0c42128598aafec4c7f784477
22BCS10271 <22BCS10271@cuchd.in>
2025-04-02 23:04:17 +0530

    Add files via upload

---
+++ Solution(207. Course Schedule).java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+
+        List<List<Integer>> graph = new ArrayList<>();
+        for (int i = 0; i < numCourses; i++)
+            graph.add(new ArrayList<>());
+
+        for (int[] pre : prerequisites) {
+            graph.get(pre[1]).add(pre[0]);
+        }
+
+        int[] visited = new int[numCourses];
+
+        for (int i = 0; i < numCourses; i++) {
+            if (dfs(graph, visited, i))
+                return false;
+        }
+        return true;
+    }
+
+    private boolean dfs(List<List<Integer>> graph, int[] visited, int course) {
+        if (visited[course] == 1)
+            return true;
+        if (visited[course] == 2)
+            return false;
+
+        visited[course] = 1;
+        for (int next : graph.get(course)) {
+            if (dfs(graph, visited, next))
+                return true;
+        }
+        visited[course] = 2;
+        return false;
+    }
+}
